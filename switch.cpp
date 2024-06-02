a10dad0398591a09fa26e227eb207f1c7a03fd3b
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-06-02 16:11:43 -0400

    Create switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+int day = 1;
+switch(year) {
+case 1:
+cout << "The day of the week is Sunday";
+break;
+case 2:
+cout << "The day of the week is Monday";
+break;
+case 3:
+cout << "The day of the week is Tuesday";
+break;
+case 4:
+cout << "The day of the week is Wednesday";
+break;
+case 5:
+cout << "The day of the week is Thursday";
+break;
+case 6:
+cout << "The day of the week is Friday";
+break;
+case 7:
+cout << "The day of the week is Saturday";
+break;
+}
+//the program will output "Sunday" because the "day" variable is set to 1, and case 1 is assigned to output "The day of the week is Sunday"
