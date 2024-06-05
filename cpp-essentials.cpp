e0cec652e474ad763299a93a75d12a955d90e9d3
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-06-05 18:24:42 -0400

    Create cpp-essentials.cpp

---
+++ cpp-essentials.cpp
@@ -0,0 +1,16 @@
+//here are some essential c++ scripts
+
+//1. basic configuration
+//these are essential to your code, and are the very first lines of a c++ file:
+#include <iostream>
+using namespace std;
+//2. main function
+//this is required for c++ applications, the code inside of the main function is the code that runs when you run the c++ file:
+int main() {
+  //your code here
+}
+//3. string import
+//if you want to use a string variable, you first will need to import the string library:
+#include <string>
+//and then you can use the string variable:
+string text = "hello";
