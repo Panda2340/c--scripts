bae4a2c9e44609875bf00f2688851e594272ce18
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-06-05 17:53:20 -0400

    Create stringvariables.cpp

---
+++ stringvariables.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string> //import the string library so you can make string variables
+using namespace std;
+int main() {
+  string text = "this is a string data value";
+}
