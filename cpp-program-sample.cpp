500801e64c2ba39af901b466638816a46dbc7af1
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-06-05 18:26:35 -0400

    Create cpp-program-sample.cpp

---
+++ cpp-program-sample.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "this is a test";
+int number = 1;
+}
+//this program prints the text "this is a test", and sets the number variable to 1.
