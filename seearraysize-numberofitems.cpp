6dce6ba2bf78fdabd65882c3dbc5db9afe8f0284
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-06-17 13:56:30 -0400

    Create seearraysize-numberofitems.cpp

---
+++ seearraysize-numberofitems.cpp
@@ -0,0 +1,8 @@
+//Normally, when you code your program so it showws the number of items in array, it displays the number of items in the array in bytes.
+//This is an example of how to display the number of items in an array:
+#include <iostream>
+using namespace std;
+int main() {
+  int items[3] = { "1", "2", "3" }; //create the array
+int getArrayLength = sizeof(items) / sizeof(int)
+}
