b945544d0229e29646faa1a3df00a7f79da824d7
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-06-18 15:50:12 -0400

    Update user-inputs.cpp

--- user-inputs.cpp
+++ user-inputs.cpp
@@ -3,4 +3,5 @@ using namespace std;
 int main() {
   int userInput; //create the variable to store the user's input
 cin >> userInput; //get the user's input
-cout << userInput;
+cout << userInput; //print the user's input;
+}
