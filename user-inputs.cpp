ce6f8dace196b9bbf8c8f204c255053e446851ec
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-06-18 15:49:38 -0400

    Create user-inputs.cpp

---
+++ user-inputs.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int userInput; //create the variable to store the user's input
+cin >> userInput; //get the user's input
+cout << userInput;
