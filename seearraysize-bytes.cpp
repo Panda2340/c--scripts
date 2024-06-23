b6d497440ef380d23c76edeee99204e21659aec4
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-06-23 14:38:23 -0400

    Update seearraysize-bytes.cpp

--- seearraysize-bytes.cpp
+++ seearraysize-bytes.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-  string array[3] = { "1", "2", "3" };
+  int array[3] = { "1", "2", "3" };
 cout << sizeof(array);
 }
 //this script displays the size of the array in bytes
