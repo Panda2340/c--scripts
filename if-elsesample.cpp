a5a6c93bf35513759ae63043817ff2c1fab6eb0e
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-06-24 18:09:43 -0400

    Update if-elsesample.cpp

--- if-elsesample.cpp
+++ if-elsesample.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
   int var = 1;
-if (var == 1) { //in the ()brackets, put the conditions
+if (var == 1) { //in the ()brackets, put the conditions for the script to occur
 cout << "The value of var is 1";
 } else {
 cout << "The value of var is not 1";
