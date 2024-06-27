fd3d871ed12298c8ecea8c21da05c3f3543a8bfd
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-06-27 18:01:45 -0400

    Update switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 using namespace std;
 int main() {
-int day = 1;
+int day = 1; //set the value of the variable to control the switch
 switch(year) {
 case 1:
 cout << "The day of the week is Sunday";
+  return 0;
 break;
 case 2:
 cout << "The day of the week is Monday";
+  return 0;
 break;
 case 3:
 cout << "The day of the week is Tuesday";
+  return 0;
 break;
 case 4:
 cout << "The day of the week is Wednesday";
+  return 0;
 break;
 case 5:
 cout << "The day of the week is Thursday";
+  return 0;
 break;
 case 6:
 cout << "The day of the week is Friday";
+  return 0;
 break;
 case 7:
 cout << "The day of the week is Saturday";
+  return 0;
 break;
 }
 //the program will output "Sunday" because the "day" variable is set to 1, and case 1 is assigned to output "The day of the week is Sunday"
