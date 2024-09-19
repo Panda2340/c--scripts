d4120c51ac9dabd282c6ad0f546b4f6fc2df1783
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-09-19 15:57:37 -0400

    Create variable-test.cpp

---
+++ variable-test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>  //Import the string function for the variable
+using namespace std;
+int main() {  //Declare the main function
+  int variable = 99;  //Create and set the intiger variable
+  string text = "Hello";  //Create and set the string variable
+}
